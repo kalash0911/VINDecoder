4bb91eb42943796f5450458d97a7b99d0a1eaf40
Yevhen <73849653+kalash0911@users.noreply.github.com>
2026-08-02 16:53:34 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // If deploying to https://<user>.github.io/<repo>/, set base to '/<repo>/'.
 // For Vercel/Netlify or https://<user>.github.io/, the default '/' is correct.
 export default defineConfig({
-  base: '/',
+  base: '/VINDecoder',
   plugins: [react()],
   build: {
     outDir: 'dist',
